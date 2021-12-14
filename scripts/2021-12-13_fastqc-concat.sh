e6025178f54dbdb8580c932780c92f127d01d510
Laura.Spencer <Laura.Spencer@nmfs.local>
2021-12-14 04:42:26 +0000

    Created slurm script for fastqc and multiqc on Sedna

---
+++ scripts/2021-12-13_fastqc-concat.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --job-name=redking_raw-fastqc
+#SBATCH --output=/home/lspencer/2022-redking-OA/sbatch_logs/redking_raw-fastqc.txt
+#SBATCH --mail-user=laura.spencer@noaa.gov
+#SBATCH --mail-type=ALL
+#SBATCH -c 2
+#SBATCH -D /share/nwfsc/ggoetz/red_king_crab/illumina/
+
+# This script is for running FastQC and MultiQC on raw (but concatenated)
+# red king crab RNA-Seq data
+
+# Load modules and virtual environments
+module load bio/fastqc
+source /home/lspencer/venv/bin/activate
+
+# run fastqc on each raw read file
+fastqc \
+*.fastqc.gz \
+--outdir /home/lspencer/2022-redking-OA/fastqc/concat/
+
+# Run multiqc to summarize fastqc reports
+multiqc \
+/home/lspencer/2022-redking-OA/fastqc/concat/ \
+--outdir /home/lspencer/2022-redking-OA/fastqc/concat/
