5a14704af397a4d3d50e0ef345a12580f671afc1
Laura.Spencer <Laura.Spencer@nmfs.local>
2022-01-18 12:47:08 -0800

    new featurecounts script for lots of options, added library prep stats csv used in R, added scxript drafts folders to relocate draft scripts

---
+++ scripts/drafts/2021-12-30_sort-deduplicate.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#SBATCH --job-name=redking_sort-dedupe
+#SBATCH --output=/home/lspencer/2022-redking-OA/sbatch_logs/redking_sort-dedupe.txt
+#SBATCH --mail-user=laura.spencer@noaa.gov
+#SBATCH --mail-type=ALL
+#SBATCH -c 4
+#SBATCH -t 5-0:0:0
+
+# This script is for sorting then deduplicating aligned RNASeq data
+
+#source /home/lspencer/venv/bin/activate
+module load bio/samtools/1.11
+module load bio/gatk/4.2.0.0
+conda activate gatk
+
+IN=/scratch/lspencer/2022-redking-OA/aligned-bowtie
+OUT=/scratch/lspencer/2022-redking-OA/aligned-bowtie/sorted-deduped
+
+#Convert .sam files to .bam files & sort by coordinate
+for file in ${IN}/*.bowtie.sam.v1
+do
+sample="$(basename -a $file | cut -d "." -f 1)"
+sorted_file="$sample.sorted.bam"
+samtools view -b $file | samtools sort -o ${OUT}/$sorted_file
+done
+
+
+# Deduplicate
+for file in ${OUT}/*.sorted.bam
+do
+sample="$(basename -a $file | cut -d "." -f 1)"
+dedup_file="$sample.dedup.bam"
+
+# deduplicate using picard, output will have duplicates removed
+gatk MarkDuplicates \
+I=$file \
+O="${OUT}/$dedup_file" \
+M="${OUT}/$sample.dup_metrics.txt" \
+REMOVE_DUPLICATES=true
+done >> "${OUT}/dedup_stout.txt" 2>&1
